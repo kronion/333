374dee33ef224dc538e9eeca93650448130a4902
Collin Stedman <cstedman@princeton>
2014-03-06 03:35:25 -0500

    Early setup for Jade and Passport

--- app.js
+++ app.js
@@ -14,12 +14,44 @@
  * =============================================================================
  */
 
+/* Secret key to be used later */
+var secret = require('./keyfile.js');
+
+/* Express */
 var express = require('express');
 var app = express();
+
+/* Static file serving */
+app.use(express.compress())
+   .use(express.static(__dirname + '/public'));
+
+/* Sessions */
+app.use(express.cookieParser())
+   .use(express.json())
+   .use(express.urlencoded())
+   .use(express.session({
+     secret: secret, 
+     key: 'sid', 
+     cookie: { 
+       secure: true
+     }
+   }));
+
+/* Passport */
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+app.use(passport.initialize())
+   .use(passport.session());
+
+/* Jade templating */
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+/* Routing */
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 app.get('/test', function(req, res) {
-  res.send('Test');
+  res.render('layout.jade');
 });
 app.listen(1337);
