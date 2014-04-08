76cd1140b04e588cea9c3603cf47521ea3be0673
Jennie Werner <JAWerner@princeton.edu>
2014-04-07 21:56:18 -0400

    fixed js files

---
+++ public/js/front.js
@@ -0,0 +1,14 @@
+/*
+	front.js
+	Holds logic for popup login.
+*/
+
+$(document).ready(function() {
+    $(".button").click(function(e) {
+    	$(".overlay, .popup").fadeToggle();
+        $("body").append(''); $(".popup").show(); 
+        $(".close").click(function(e) { 
+            $(".popup, .overlay").hide(); 
+        }); 
+    }); 
+});
