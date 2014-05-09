d4b65b3a640adc8fd3d6a207183aaec0622ee2cf
Collin Stedman <cstedman@princeton>
2014-05-08 23:40:18 -0400

    Fixed Erik's merge, everything works

---
+++ email.js
@@ -0,0 +1,11 @@
+module.exports = {
+  from: 'cstedman@princeton.edu',
+  host: 'smtp.gmail.com',
+  secureConnection: true,
+  port: 465,
+  transportMethod: 'SMTP',
+  auth: {
+    user: 'cstedman@princeton.edu',
+    pass: 'am1n0Ac1d1!'
+  }
+};
